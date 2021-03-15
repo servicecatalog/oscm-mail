6e0ff5dae77340548771ec3a5631451c6bfe573e
kowalczyka <arkadiusz.kowalczyk@ts.fujitsu.com>
2021-03-15 09:58:57 +0100

    Added auth endpoint and context path

--- src/main/java/org/oscm/mail/configuration/SecurityConfiguration.java
+++ src/main/java/org/oscm/mail/configuration/SecurityConfiguration.java
@@ -23,7 +23,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
 
     http.authorizeRequests()
-        .antMatchers("/data/emails")
+        .antMatchers("/data/emails", "/auth/")
         .permitAll()
         .anyRequest()
         .authenticated()
--- src/main/java/org/oscm/mail/controller/MainController.java
+++ src/main/java/org/oscm/mail/controller/MainController.java
@@ -10,6 +10,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.UnknownHostException;
 import java.util.List;
 
 @Controller
@@ -18,9 +21,9 @@ public class MainController {
   @Autowired private AuthenticationContext authContext;
 
   @RequestMapping("/")
-  public String index() {
-
-    return "index";
+  public String index() throws UnknownHostException {
+    String host = InetAddress.getLocalHost().getHostName();
+    return "redirect:https://" + host + "/mail";
   }
 
   @RequestMapping("/email")
@@ -28,7 +31,7 @@ public class MainController {
 
     EmailProvider provider = new MaildevEmailProvider(new RestTemplate());
     List<Email> emails = provider.getAllEmails();
-    
+
     model.addAttribute("emails", emails);
     return "emails";
   }
