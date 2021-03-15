94b97288b5f59e5031087b3f6dad3793d6adf9d3
kowalczyka <arkadiusz.kowalczyk@ts.fujitsu.com>
2021-03-15 10:17:39 +0100

    Modified context path

--- src/main/java/org/oscm/mail/controller/MainController.java
+++ src/main/java/org/oscm/mail/controller/MainController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.InetAddress;
-import java.net.NetworkInterface;
 import java.net.UnknownHostException;
 import java.util.List;
 
